ff54af6a00724414ecc8aae0f4217e4388a44bdc
Franklin-Gomez <franklin_043097@hotmail.com>
2024-04-29 09:37:24 -0500

    add state to inputs

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,12 +1,20 @@
 import { categories } from "../db/categorias"
+import { useState } from "react"
 
 export default function Form() {
+
+  const [ activity , setActivity ] = useState({
+    category : 1,
+    name : '',
+    calorias : 0
+  })
+
   return (
 
     <form action="" className='space-y-5 bg-white p-10 rounded-lg'>
       <div className='grid grid-cols-1 gap-3'>
         <label htmlFor='category'> Categoria : </label>
-        <select className=' border border-slate-300 p-2 rounded-lg w-full bg-white' id='category'>
+        <select className=' border border-slate-300 p-2 rounded-lg w-full bg-white' id='category' value={activity.category}>
 
           { categories.map( ( categoria ) => (
             <option
@@ -21,24 +29,26 @@ export default function Form() {
         </select>
 
         <div className="grid grid-cols-1 gap-3">
-          <label htmlFor="activity" className="font-bold "> Actividad : </label>
+          <label htmlFor="name" className="font-bold "> Actividad : </label>
           
           <input 
-            id="activity"
+            id="name"
             type="text"
             className="border border-slate-300 p-2 rounded-lg"
             placeholder="Ej. Comida , Jugo de Naranja , Ensadla , Ejercico , Pesas , Bicicleta"
+            value={activity.name}
           />
         </div>
 
         <div className="grid grid-cols-1 gap-3">
-          <label htmlFor="activity" className="font-bold "> Calorias : </label>
+          <label htmlFor="calorias" className="font-bold "> Calorias : </label>
           
           <input 
             id="calorias"
             type="number"
             className="border border-slate-300 p-2 rounded-lg"
             placeholder="Ej. cantidad aproximadas 100 , 200..."
+            value={activity.calorias}
           />
         </div>
 
