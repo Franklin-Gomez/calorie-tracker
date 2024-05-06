977595e125377fccf6b9cef95bcae888dfb25c80
Franklin-Gomez <franklin_043097@hotmail.com>
2024-05-06 10:12:50 -0500

    feature mensaje de no hay actividades condicional

--- src/Components/ActivityList.tsx
+++ src/Components/ActivityList.tsx
@@ -11,54 +11,61 @@ type ActivityListProps = {
 export default function ActivityList( { activities , dispatch  } : ActivityListProps) {
 
     // formatear categoria de 1 : comida  2 : ejercicio
-    const categoryName = useMemo( () => 
-        (category : Activity['category']) => categories.map( cat => cat.id === category ? cat.name : '') , [activities]);
+    const categoryName = useMemo( () => (category : Activity['category']) => categories.map( cat => cat.id === category ? cat.name : '') , [activities]);
+
+    const isEmptyActivities = useMemo( () => activities.length === 0 , [ activities ])
 
     return (
         <>
             <h2 className="text-4xl font-bold text-slate-600 text-center"> Comida y Actividades </h2>
 
-            { activities.map( activity => ( 
+            { isEmptyActivities ? 
 
-                <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
+                <p className=" text-center"> No hay Actividades Aun </p> :
 
-                    <div className="space-y-2- relative">
+                activities.map( activity => ( 
 
-                        <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category == 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
-                            {categoryName(+activity.category)}
-                        </p>
+                    <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
 
-                        <p className="text-2xl font-bold pt-5"> { activity.name } </p>
-                        <p className="font-black text-4xl text-lime-500">
-                            {activity.calorias}{' '}
-                            <span>Calorias</span>
-                        </p>
-                        
+                        <div className="space-y-2- relative">
+
+                            <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category == 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                                {categoryName(+activity.category)}
+                            </p>
+
+                            <p className="text-2xl font-bold pt-5"> { activity.name } </p>
+                            <p className="font-black text-4xl text-lime-500">
+                                {activity.calorias}{' '}
+                                <span>Calorias</span>
+                            </p>
+                            
+                        </div>
+
+                        <div className="flex gap-5 items-center">
+                            <button
+                                onClick={ () => dispatch({type : "set-activeId" , payload : { id : activity.id }})}
+                            >
+                                <PencilSquareIcon
+                                    className="h-8 w-8 text-gray-800"
+                                />
+                            </button>
+
+                            <button
+                                onClick={ () => dispatch({type : "delete-activity" , payload : { id : activity.id }})}
+                            >
+                                <XCircleIcon
+                                    className="h-8 w-8 text-red-500"
+                                />
+                            </button>
+
+                        </div>
+
                     </div>
 
-                    <div className="flex gap-5 items-center">
-                        <button
-                            onClick={ () => dispatch({type : "set-activeId" , payload : { id : activity.id }})}
-                        >
-                            <PencilSquareIcon
-                                className="h-8 w-8 text-gray-800"
-                            />
-                        </button>
+                ))
 
-                        <button
-                            onClick={ () => dispatch({type : "delete-activity" , payload : { id : activity.id }})}
-                        >
-                            <XCircleIcon
-                                className="h-8 w-8 text-red-500"
-                            />
-                        </button>
+            }
 
-                    </div>
-
-
-                </div>
-
-            ))}
         </>
     )
 }
