d0099786749735067c7a7ab12aa60a9fbaa0e6d6
Franklin-Gomez <franklin_043097@hotmail.com>
2024-05-06 14:40:38 -0500

    working on CaloriaTracker.tsx

--- src/Components/CaloriaTraker.tsx
+++ src/Components/CaloriaTraker.tsx
@@ -1,12 +1,29 @@
+import { useMemo } from "react"
+
 type CaloriaTrakerProps = {
     activities : Activity[]
 }
 
 
 export default function CaloriaTraker({ activities } : CaloriaTrakerProps) {
-  return (
-    <div>
-        <h1> Calorias Tracker</h1>
-    </div>
-  )
+
+    // Contadores
+    const caloriasConsumed = useMemo(() => activities.reduce( (total , activity ) => activity.category === 1 ? total + +activity.calorias : total, 0 ) , [activities])
+
+    return (
+        <div>
+            <>
+                <h2 className="text-4xl font-black text-white text-center"> Resumen de Calorias </h2>
+
+                <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+                    <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+
+                        <span className="font-black text-6xl text-orange"> { caloriasConsumed }</span>
+                        Consumidas
+                    </p>
+                    
+                </div>
+            </>
+        </div>
+    )
 }
