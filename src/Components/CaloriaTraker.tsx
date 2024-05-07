75ba50f5af065b65947f89695f88b8d947c5506e
Franklin-Gomez <franklin_043097@hotmail.com>
2024-05-07 08:46:37 -0500

    calorias burned

--- src/Components/CaloriaTraker.tsx
+++ src/Components/CaloriaTraker.tsx
@@ -10,6 +10,9 @@ export default function CaloriaTraker({ activities } : CaloriaTrakerProps) {
     // Contadores
     const caloriasConsumed = useMemo(() => activities.reduce( (total , activity ) => activity.category === 1 ? total + +activity.calorias : total, 0 ) , [activities])
 
+    const caloriasBurned = useMemo(() => activities.reduce( (total , activity ) => +activity.category === 2 ? total + +activity.calorias : total, 0 ) , [activities])
+
+    console.log( activities )
     return (
         <div>
             <>
@@ -17,11 +20,14 @@ export default function CaloriaTraker({ activities } : CaloriaTrakerProps) {
 
                 <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
                     <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-
                         <span className="font-black text-6xl text-orange"> { caloriasConsumed }</span>
                         Consumidas
                     </p>
-                    
+
+                    <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+                        <span className="font-black text-6xl text-orange"> { caloriasBurned }</span>
+                        Quemadas
+                    </p> 
                 </div>
             </>
         </div>
